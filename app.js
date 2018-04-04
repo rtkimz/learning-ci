88f9a420e35f856d8c5dc30cbe6e276c3e717e41
robin <robinkimzey@gmail.com>
2018-04-03 17:45:28 -0700

    second check to see if Jenkins is working

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World - is Jenkins working?");
+  res.send("Hello World - is Jenkins(2) working?");
 });
 
 app.listen(4000);
