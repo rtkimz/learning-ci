926a59b60b22808c04078923c474eb509c63225c
robin <robinkimzey@gmail.com>
2018-04-12 18:24:39 -0700

    fixes Jenkins setup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World! trying to fix Jenkins builds/tests'))
+app.get('/', (req, res) => res.send('Hello World! Jenkins builds/tests, YEAH!!!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
